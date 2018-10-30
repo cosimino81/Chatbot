a2bda48c79a1f5284933f66066b81e07106da5e8
curiacosi1 <Cosimo.CURIALE@guest.ferrero.com>
2018-10-30 17:37:43 +0100

    New code

--- Deep-NLP/chatbot.py
+++ Deep-NLP/chatbot.py
@@ -30,7 +30,7 @@ for conversation in conversations[:-1]:  # The last row is empty
     conversation_ids.append(_conversation.split(','))
 
 # Getting seperately the questions and the answers
-# NOTE: In the 'conversation_ids' lis the first code of every element is the answer
+# NOTE: In the 'conversation_ids' list the first code of every element is the question
 #       and the second code is the answer
 questions = []
 answers = []
@@ -66,10 +66,13 @@ def clean_text(text):
     text = re.sub(r"\'re", " are", text)
     text = re.sub(r"\\", "", text)
     text = re.sub(r"/", "", text)
-    text = re.sub(r"[-|'.?,;:<>{}!]", "", text)
-    text = re.sub(r'[-#"...]', "", text)
+    text = re.sub(r"\[", "", text)
+    text = re.sub(r"\]", "", text)
+    text = re.sub(r'"', '', text)
+    text = re.sub(r"[-|'.&?*,;:<>{}!]", "", text)
+    text = re.sub(r'[-#%".]', "", text)
     text = re.sub(r"[-@+=-]", "", text)
-    text = re.sub(r"[-()]", "", text)
+    text = re.sub(r"[-()$]", "", text)
     text = re.sub(r"  ", " ", text)
     text = text.strip()
     return text
@@ -98,9 +101,95 @@ for answer in clean_answers:
         if word not in word2count:
             word2count[word] = 1
         else:
-            word2count[word] += 1
+            word2count[word] +=1
+
+# FILTERING AND TOKENIZATION 
+# Creating two dictionaries that map the questions words and the answer words to unique integer
+threshold = 20
+questionwords2int ={}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        questionwords2int[word] = word_number
+        word_number +=1
+        
+answerwords2int ={}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        answerwords2int[word] = word_number
+        word_number +=1
+
+# Adding the last tokens to the two dictionaries
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    questionwords2int[token] = len(questionwords2int) + 1
+for token in tokens:
+    answerwords2int[token] = len(answerwords2int) + 1 
 
 
+
+# Creating the inverse dictionary of the answerwords2int dictionary
+answerints2word = {w_i: w for w, w_i in answerwords2int.items()}
+
+# Adding the End of String token to the end of every answer
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>' 
+
+# Translate all the questions and answers (clean_answers, clean_questions) 
+# into the unique integer that was mapped into answerints2word
+# and replacing all the words that were filtered out with <OUT>
+questions_to_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionwords2int:
+            ints.append(questionwords2int['<OUT>'])
+        else:
+            ints.append(questionwords2int[word])
+    questions_to_int.append(ints)
+        
+
+answers_to_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerwords2int:
+            ints.append(answerwords2int['<OUT>'])
+        else:
+            ints.append(answerwords2int[word])
+    answers_to_int.append(ints)   
+
+# Sorting both the questions and answers by the lenght of the questions
+sorted_clean_questions = []
+sorted_clean_answers = []
+for length in range(1, 25 + 1):
+    for i in enumerate(questions_to_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_to_int[i[0]])
+            sorted_clean_answers.append(answers_to_int[i[0]])
+    
+
+############# PART 2 - BUILDING THE SEQ2SEQ MODEL ##############
+
+# Creating the placeholders for input and targhets
+def model_inputs():
+    # Arguments: placeholder(type of input, matrix dimension, name of input)
+    inputs = tf.placeholder(tf.int32, [None, None], name = 'input')
+    targets = tf.placeholder(tf.int32, [None, None], name = 'target')
+    lr = tf.placeholder(tf.float32, name = 'learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
+    return inputs, targets, lr, keep_prob
+
+# Processing the targets
+def preprocess_targets(targets, word2int, batch_size):
+    # Arguments: tf.fill(size of the matrix, )
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets
+
+# Creating the Encoder RNN Layer           
 
 
 
